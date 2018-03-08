d997ffe40c992001f82c3309333649eaad89d4d1
ishanu <ishanu.chattopadhyay@gmail.com>
2018-03-07 21:21:36 -0600

    upd

--- bin/script1.py
+++ bin/script1.py
@@ -14,15 +14,22 @@ def getModel(jsonfile,n=5):
     M=models(jsonfile)
     M.augmentDistance()
     return M.select(var='delay',n=n,reverse=False)
-    
+
+
 N=2
 path='../data/'
 jsonFiles = [f for f in os.listdir(path) if f.endswith('.json')]
 M0={}
 
+
+str_=np.arange(99,110).astype('string')
+files=[path+'Q_'+i+'.json' for i in str_]
+print files
+
 for f in jsonFiles:
-    print f
-    M0.update(getModel(path+f,n=N))
+    if f in files:
+        print f
+        M0.update(getModel(path+f,n=N))
 
 sp.to_json(M0,'models'+str(N)+'.json')
     
