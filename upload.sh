ca1ef4869a4d52049e6a96c993241556a5047684
ishanuc <ishanu.chattopadhyay@gmail.com>
2018-08-22 20:50:19 -0500

    upload

--- upload.sh
+++ upload.sh
@@ -2,13 +2,18 @@
 
 COMMENT=" updated "
 
+ONLYTEST=0
 
 
 if [ $# -gt 0 ] ; then
     COMMENT=$1
 else
-    echo "USAGE: ./upload.sh <comment>"
+    echo "USAGE: ./upload.sh <comment> <only_test>"
+    echo "if only_test is 1, then upload to only test"
     exit
+fi
+if [ $# -gt 1 ] ; then
+    ONLYTEST=$2
 fi
 
 a=$((`awk -F= '{print $2}' version.py  | sed "s/\s*'//g" | awk -F. '{print $NF}'` + 1))
@@ -24,4 +29,7 @@ echo __version__ = \'$VERSION\' > version.py
 git tag $VERSION -m $COMMENT
 git push --tags
 python setup.py sdist upload -r test
-python setup.py sdist upload -r pypi
+
+if [ ONLYTEST -eq 0 ] ; then
+    python setup.py sdist upload -r pypi
+fi
