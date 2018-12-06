339db13e24a7714e528dd04a3c6929243afbcf37
Timmy <timmyli530@gmail.com>
2018-12-06 13:11:16 -0600

    fixed setup

--- setup.py
+++ setup.py
@@ -58,7 +58,6 @@ setup(
                 ('example_cynet/example2/models/',['example_cynet/example2/models/empty.txt']),
                 ('example_cynet/example2/triplet/',['example_cynet/example2/triplet/empty.txt']),
                 ('example_cynet/example2/split/',['example_cynet/example2/split/empty.txt']),
-                ('example_cynet/',['example_cynet/example1.tar.xz']),
-                ('bin/',['cynet/bin/cynet','cynet/bin/flexroc','cynet/bin/XgenESeSS'])],
+                ('example_cynet/',['example_cynet/example1.tar.xz'])],
     include_package_data=True
 )
